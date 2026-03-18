1670ad652bb7136b282fc6d9b0070f307ad4251c
TRUPTI135 <truptisuppan99@gmail.com>
2026-03-18 14:36:32 +0530

    added our product carousel section in homepage

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import "./ProductSlider.css";
-
 
 import product1 from "../assets/BeadMill.png";
 import product2 from "../assets/ButterflyMixer.png";
@@ -9,38 +7,32 @@ import product4 from "../assets/PugMixer.png";
 import product5 from "../assets/RibbonBlender.png";
 import product6 from "../assets/TwinShaftMixer.png";
 
+const products = [
+  { img: product1, title: "Bead Mill" },
+  { img: product2, title: "Butterfly Mixer" },
+  { img: product3, title: "High Speed Dissolver" },
+  { img: product4, title: "Pug Mixer" },
+  { img: product5, title: "Ribbon Blender" },
+  { img: product6, title: "Twin Shaft Mixer" },
+];
 
 const ProductSlider = () => {
   return (
     <section className="product-slider">
       <h2>Our Products</h2>
 
-      <div className="slider-track">
-        <div className="slide">
-          <img src={product1} alt="product1" />
-        </div>
-        <div className="slide">
-          <img src={product2} alt="product2" />
-        </div>
-        <div className="slide">
-          <img src={product3} alt="product3" />
-        </div>
-        <div className="slide">
-          <img src={product4} alt="product4" />
-        </div>
-
-        {/* duplicate for smooth infinite sliding */}
-        <div className="slide">
-          <img src={product5} alt="product1" />
-        </div>
-        <div className="slide">
-          <img src={product6} alt="product2" />
-        </div>
-        <div className="slide">
-          <img src={product3} alt="product3" />
-        </div>
-        <div className="slide">
-          <img src={product4} alt="product4" />
+            <div className="slider-wrapper">
+        <div className="slider">
+          <div className="slider-track">
+            {[...products, ...products].map((item, index) => (
+              <div className="card" key={index}>
+                <div className="image-box">
+                  <img src={item.img} alt={item.title} />
+                </div>
+                <h3>{item.title}</h3>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,8 +27,7 @@ function Home() {
 
      
 
-      {/* Footer */}
-      <Footer />
+      
     </>
   );
 }
