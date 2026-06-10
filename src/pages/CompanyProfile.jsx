04d67878ab4e9db7ebf4e0a2c3814a59a1f3fd46
vmp0059 <vaishnavimpatil.987@gmail.com>
2026-06-10 19:24:16 +0530

    Company data for company page

--- src/pages/CompanyProfile.jsx
+++ src/pages/CompanyProfile.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../components/CompanyPage.css';
+import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import CapbilitiesSection from '../components/CapabilitiesSection';
+import JourneyAndWhySection from '../components/JourneyAndWhySection';
+import CTABanner  from '../components/CTABanner';
+
 
 export default function CompanyProfile() {
     return (
-        <div className="company-profile">
-            <h1>Company Profile</h1>
-            <p>Welcome to the Company Profile page.</p>
+        <div>
+            <HeroSection />
+            <AboutSection />
+            <CapbilitiesSection />
+            <JourneyAndWhySection />
+            <CTABanner/>
+            
         </div>
     );
 }
